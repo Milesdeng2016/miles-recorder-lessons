c7cf860484d6d0bdd3d4a71035c111a43d444fbf
Miles Deng <milesdeng2016@mac.home>
2025-01-28 20:56:08 -0500

    style: add some style

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,8 +6,15 @@ export default function Home() {
   });
 
   return (
-    <div className="w-[100vw] h-[100vh] flex items-center justify-center">
-      <div>{recording ? "Recording" : "Not Recording"}</div>
+    <div className="w-[100vw] h-[100vh] p-4 bg-black">
+      <div className="rounded-2xl h-full ring-1 ring-zinc-500 px-8 py-8 flex flex-col">
+        <div className="w-full flex items-center justify-between pb-4">
+          <div className="flex items-center text-2xl font-semibold leading-none tracking-tight text-white">
+            Miles's Recorder
+          </div>
+          <div>{recording ? "Recording" : "Not Recording"}</div>
+        </div>
+      </div>
     </div>
   );
 }
